78bc794c1ad8118e2a08ccf5b9ef9e34f6167fdc
DexZed <144045080+DexZed@users.noreply.github.com>
2025-07-03 11:35:39 +0600

    `Added react-router and related dependencies, refactored App component to use Nav, ListView, and Footer components, and integrated react-router into main.tsx.`

--- Assignment 4/src/main.tsx
+++ Assignment 4/src/main.tsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+import { createBrowserRouter, RouterProvider } from "react-router";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "contact",
+        element: <div></div>,
+      },
+    ],
+  },
+]);
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
